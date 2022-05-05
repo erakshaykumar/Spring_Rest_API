732c5d4135a862636a6b56ceab47b94eb866bacc
erakshaykumar <akaakshay304@gmail.com>
2022-05-05 12:33:55 +0530

    Added: Use GET Request Method and pass name as query parameter

--- src/main/java/com/springrestapi/controller/HelloRestController.java
+++ src/main/java/com/springrestapi/controller/HelloRestController.java
@@ -4,7 +4,9 @@ package com.springrestapi.controller;
  * Create a Rest Controller to demonstrate the various HTTP Methods and respond hello messages to the User.
  */
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 
@@ -20,4 +22,13 @@ public class HelloRestController {
         return "Hello Form BridgeLabz";
     }
 
+    /**
+     * @RequestParam : Mapping HTTP GET Request onto specific Handler Methods.
+     *                 When mapping is done all statements of  sayHello method get executed
+     *  curl : localhost:8080/getMessage?name=Akshay
+     */
+    @GetMapping(value="/getMessage")
+    public String sayHello(@RequestParam(value = "name" )String name){
+        return "Hello " + name + " Welcome To BridgeLabz";
+    }
 }
