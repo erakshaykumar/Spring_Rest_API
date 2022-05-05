4b745c5d3c11c66845d31943660b8e76dc390693
erakshaykumar <akaakshay304@gmail.com>
2022-05-05 12:39:56 +0530

    Added: Use GET Request Method And Pass Name As Path Variable

--- src/main/java/com/springrestapi/controller/HelloRestController.java
+++ src/main/java/com/springrestapi/controller/HelloRestController.java
@@ -4,10 +4,7 @@ package com.springrestapi.controller;
  * Create a Rest Controller to demonstrate the various HTTP Methods and respond hello messages to the User.
  */
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 
 @RestController
@@ -29,6 +26,16 @@ public class HelloRestController {
      */
     @GetMapping(value="/getMessage")
     public String sayHello(@RequestParam(value = "name" )String name){
+        return "Hello " + name + " Welcome To BridgeLabz";
+    }
+
+     /** Purpose : Use GET Request Method and pass name as path variable
+     * @param name
+     * @return
+     * localhost:8080/param/Akshay%20Kumar
+     */
+    @GetMapping(value= "/param/{name}")
+    public String sayHelloParam(@PathVariable String name){
         return "Hello " + name + " Welcome To BridgeLabz";
     }
 }
